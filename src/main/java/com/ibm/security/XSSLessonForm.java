4046c31a39a45972f4e57d3c400e6687541a4892
Madhu Nandyala <madhunandyala@in.ibm.com>
2019-05-02 14:18:24 +0530

    Initial Commit

---
+++ src/main/java/com/ibm/security/XSSLessonForm.java
@@ -0,0 +1,29 @@
+package com.ibm.security;
+
+import org.apache.struts.validator.ValidatorForm;
+
+public class XSSLessonForm extends ValidatorForm {
+
+	private String userName;
+		
+	public XSSLessonForm() {
+		super();
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		System.out.println("Inside getter " + userName);
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		System.out.println("Inside setter " + userName);
+		this.userName = userName;
+	}
+
+}
